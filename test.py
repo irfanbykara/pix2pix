cd4b3b9c4f1c2abcb8c50ef4bfacd821782e9353
İrfan Baykara <63783207+irfanbykara@users.noreply.github.com>
2025-05-29 20:09:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ from utils.facade_dataloader import get_test_dataloader  # You must implement th
 
 import os
 def test(model_path, save_dir):
-    G = Generator([64, 128, 256, 512, 512, 512, 512], 512)
-    G.load_state_dict(torch.load(model_path, map_location='cpu'))
-    G.eval()
+    generator = Generator([64, 128, 256, 512, 512, 512, 512], 512)
+    generator.load_state_dict(torch.load(model_path, map_location='cpu'))
+    generator.eval()
 
     dataloader = get_test_dataloader()
     with torch.no_grad():
         for idx, (input_image, _) in enumerate(dataloader):
-            fake = G(input_image)
+            fake = generator(input_image)
             save_image(fake, f"{save_dir}/output_{idx}.png")
 
 if __name__ == "__main__":
